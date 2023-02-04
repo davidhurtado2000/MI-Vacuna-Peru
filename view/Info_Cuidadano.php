d9f4526baae27e5a187bdc11cf528ad998c69c13
davidhurtado2000 <david.huetado2000@gmail.com>
2023-02-03 20:39:56 -0500

    update-ihtmlbase

---
+++ view/Info_Cuidadano.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <title>Mi Vacuna Peru</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="../css/login.css" type="text/css" rel="stylesheet" media="">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css"
+        integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+</head>
+
+<header class="container-fluid" id="navegador">
+    <nav class="navbar navbar-light bg-light">
+        <p class="h1">Mi Vacuna Peru <img src="../img/peru.png" alt="Responsive image" id="logo_peru"></p>
+        <img src="../img/logo_minsa.png" class="img-fluid" alt="Responsive image" id="minsa">
+    </nav>
+</header>
+
+<body>
+
+    <div class="col-lg-12" style="background-color: aqua;">Loperum</div>
+
+</body>
+
+<footer>
+
+</footer>
+
+</html>
