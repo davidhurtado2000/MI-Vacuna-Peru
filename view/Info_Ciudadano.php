9b8ed87f35ba6efee55ba1c204ed1f352348bbfa
davidhurtado2000 <david.huetado2000@gmail.com>
2023-02-05 22:23:49 -0700

    update

--- view/Info_Ciudadano.php
+++ view/Info_Ciudadano.php
@@ -14,7 +14,7 @@
 
 
 <header>
-    <div class="container-fluid" style="background-color: blue;">
+    <div class="container-fluid" >
         <div class="row">
             <div class="col-lg-6">
                 <div class="float-start">
@@ -32,7 +32,7 @@
 <body>
 
     <div class="container-fluid mt-3 px-4 py-4" style="background-color: red;">
-        <div class="container-fluid" style="background-color: aquamarine;">
+        <div class="container-fluid pb-4 " style="background-color: #ffe599;">
             <div class="row">
                 <div class="col-lg-6">
                     <div class="float-start">
@@ -47,18 +47,30 @@
             </div>
             <div class="row">
                 <div class="col-lg-2">
-                    <div class="btn-group-vertical" style="width: 100%; background-color:white">
-                        <button type="button" class="btn border border-dark rounded-0 bg-light">Informacion del
-                            Usuario</button>
-                        <button type="button" class="btn border border-dark rounded-0">Historial de Vacunas</button>
-                        <button type="button" class="btn border border-dark rounded-0">Vacunas Pendientes</button>
-                        <button type="button" class="btn border border-dark rounded-0">Buscador de Vacunas</button>
+                    <div class="btn-group-vertical " style="width: 100%; background-color:white;">
+                        <div type="button" class="btn change-bgcolor border border-dark rounded-0 py-4" >
+                            <img src="../img/i_icon.png" alt="Responsive image" id="menu_logo" class="float-start">
+                            <p class="text-wrap">Informacion del Usuario</p>
+                        </div>
+                        <div type="button" class="btn change-bgcolor border border-dark rounded-0 py-4" href="#">
+                            <img src="../img/historial_icon.png" alt="Responsive image" id="menu_logo" class="float-start">
+                            <p class="text-wrap">Historial de Vacunas</p>
+                        </div>
+                        <div type="button" class="btn change-bgcolor border border-dark rounded-0 py-4">
+                            <img src="../img/pendiente_icon.png" alt="Responsive image" id="menu_logo" class="float-start">
+                            <p class="text-wrap">Vacunas Pendientes</p>
+                            
+                        </div>
+                        <div type="button" class="btn change-bgcolor border border-dark rounded-0 py-4">
+                            <img src="../img/buscador_icon.png" alt="Responsive image" id="menu_logo" class="float-start">
+                            <p class="text-wrap">Buscador de Vacunas</p>
+                        </div>
                     </div>
                 </div>
-                <div class="col-lg-10" style="background-color: blueviolet;">
-                <div class="col-md-12 px-4 py-4" style="background-color: purple;">
-                    Algo
-                </div>
+                <div class="col-lg-10">
+                    <div class="col-md-12 px-4 h-100 border border-dark rounded-0 overflow-auto" style="background-color: white; max-height: 357px;">
+                        <p>Algo</p>
+                    </div>
                 </div>
             </div>
         </div>
