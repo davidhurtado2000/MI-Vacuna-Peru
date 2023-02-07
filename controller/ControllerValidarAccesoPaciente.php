7b72a48bb78bcfe3bf603426f55ed5f4bbedfd20
davidhurtado2000 <david.huetado2000@gmail.com>
2023-02-07 01:04:11 -0700

    update-login

--- controller/ControllerValidarAccesoPaciente.php
+++ controller/ControllerValidarAccesoPaciente.php
@@ -2,6 +2,15 @@
 session_start();
 include 'ControllerPaciente.php';
 
+function ageCalculator(String $bday) {
+    $newDate = date("d-m-Y", strtotime($bday));  
+    $bdayc = new DateTime($newDate);
+    $today = new Datetime(date('d.m.y'));
+    $diff = $today->diff($bdayc);
+    $result = $diff->y;
+    return $result;
+    }     
+
 $pro = new ControllerPaciente();
 IF($_POST["dni"] == "" || $_POST["emision"] == "" || $_POST["nacimiento"] == ""){
     header('Location:../Log-in.php?err=1');
@@ -16,11 +25,19 @@ IF($_POST["dni"] == "" || $_POST["emision"] == "" || $_POST["nacimiento"] == "")
         $_SESSION['nombres'] = $fila['nombres'];
         $_SESSION['a_paterno'] = $fila['apellido_p'];
         $_SESSION['a_materno'] = $fila['apellido_m'];
-        $_SESSION['edad'] = $fila['']; //Hallar edad
+        $_SESSION['edad'] = ageCalculator($fila['f_nacimiento']);
         $_SESSION['direccion'] = $fila['direccion'];
 
 
     }
 }
+
+/*
+Codigo para hallar la edad
+$bday = new DateTime('13.12.2000'); // Your date of birth
+$today = new Datetime(date('d.m.y'));
+$diff = $today->diff($bday);
+printf(' Your age : %d years, %d month, %d days', $diff->y, $diff->m, $diff->d);
+printf("\n");*/
 
 ?>
